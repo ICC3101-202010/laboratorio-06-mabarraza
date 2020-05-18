273c64dfa70d0e624d1b94edd6d7d55d7032e38e
mabarraza <mabarraza@miuandes.cl>
2020-05-18 16:07:52 -0400

    first commit

---
+++ Laboratorio_6/Laboratorio_6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laboratorio_6
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
